4b46d43c756177766acac79cf363afa7ee748f21
Austin Spinazze <spinazzeaustin@yahoo.com>
2021-03-26 15:20:00 -0500

    Initial SpringBoot project

---
+++ src/test/java/austinspinazze/flowablespringboot/FlowableSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package austinspinazze.flowablespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
